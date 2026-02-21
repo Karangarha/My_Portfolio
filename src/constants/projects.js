5d855cf6eb6836d43ccb3f42928212a7e2bb7fc5
Karangarha <garhasaab001@gmail.com>
2026-02-20 22:40:00 -0500

    updated project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -4,17 +4,26 @@ export const projects = [
         name: "Restaurant Management Platform",
         description: "Built with Supabase Realtime and React for seamless dining operations.",
         image: "https://res.cloudinary.com/dns6xkqht/image/upload/v1771632734/RMS_nzabbd.png",
-        github: "https://github.com/your-repo",
-        live: "https://live-site.com",
-        tags: ["React", "Supabase", "Node.js"] // Helpful for those cards we built
+        github: "https://github.com/Karangarha/Constance-s-Cuisine",
+        live: "https://constance-s-cuisine.vercel.app/",
+        tags: ["React", "Supabase", "Node.js", "RESTful APIs", "Vercel", "CI/CD", "PostgreSQL"] // Helpful for those cards we built
     },
     {
         id: 2,
-        name: "ACM Chapter Kean Website",
+        name: "ACM Chapter Kean Frontend",
         description: "Revamped the official Kean ACM chapter website with a focus on UX and performance.",
         image: "https://res.cloudinary.com/dns6xkqht/image/upload/v1771617354/KeanACM_Project_r96aee.png",
-        github: "https://github.com/kean-acm",
-        live: "https://acm.kean.edu",
-        tags: ["TypeScript", "Tailwind", "Vite"]
+        github: "https://github.com/Karangarha/ACM_Kean",
+        live: "https://www.keanacm.com/",
+        tags: ["TypeScript", "Tailwind", "React", "Vercel", "CI/CD", "Cloudinary"]
+    },
+    {
+        id: 3,
+        name: "ACM Chapter Kean Backend",
+        description: "Revamped the official Kean ACM chapter website with a focus on UX and performance.",
+        image: "https://res.cloudinary.com/dns6xkqht/image/upload/v1771617354/KeanACM_Project_r96aee.png",
+        github: "https://github.com/Karangarha/Kean_ACM_Backend",
+        live: "https://www.keanacm.com/",
+        tags: ["Node.js", "express", "MongoDB", "JWT", "RBAC", "RESTful APIs", "Azure DevOps", "CI/CD", "Cloudinary"]
     }
 ];
