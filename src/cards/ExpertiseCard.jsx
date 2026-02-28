08b18727fc7939e00af180fedf2b54ce790176d2
Karangarha <garhasaab001@gmail.com>
2026-02-28 00:56:17 -0500

    updated CSS properties

--- src/cards/ExpertiseCard.jsx
+++ src/cards/ExpertiseCard.jsx
@@ -4,7 +4,7 @@ import { Code, MonitorCog, UserStar } from "lucide-react";
 const ExpertiseCard = ({ icon: Icon, name, expertise }) => {
   return (
     <motion.div
-      className="bg-background p-4 flex flex-rows gap-2"
+      className="bg-background p-4 flex flex-rows gap-2 rounded-xl"
       whileHover={{
         scale: 1.05,
         boxShadow: "1px 1px 20px 0px var(--color-primary)",
